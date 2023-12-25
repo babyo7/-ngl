ce240b4b05ff8f2b6336990bc8b8aa5f7c8296d5
Aman <119067199+babyo7@users.noreply.github.com>
2023-12-25 20:25:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const router = require("./routes/router");
+const path = require('path');
 const { notFound } = require("./models/pages");
 
 const PORT = process.env.PORT || 3000;
