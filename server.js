ecbc7029ab6057d360984ea9a3043c3710b070ee
Aman <119067199+babyo7@users.noreply.github.com>
2023-12-25 20:24:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { notFound } = require("./models/pages");
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 
